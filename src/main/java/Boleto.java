6d2ffefcfcf5a755843bfbf0547ca8129077517c
Lucas Menezes <lucasmezs@outlook.com>
2024-04-02 20:53:22 -0300

    main.java

---
+++ src/main/java/Boleto.java
@@ -0,0 +1,16 @@
+
+public class Boleto {
+
+  public void emitirBoleto() {
+    System.out.println("Emitindo boleto...");
+  }
+
+  public void cancelarBoleto() {
+    System.out.println("Cancelando boleto...");
+  }
+
+  public void receberBoleto() {
+    System.out.println("Recebendo boleto...");
+  }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Boleto variavelBoleto = new Boleto();
+    variavelBoleto.emitirBoleto();
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
+}
